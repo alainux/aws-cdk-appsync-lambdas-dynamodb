1170b35ac63e34aacfc7d6844ac5ef180e5cc069
Alain <alain@pro.local>
2022-09-08 21:23:31 -0500

    Add dynamodb table

--- lib/book-store-graphql-api-stack.ts
+++ lib/book-store-graphql-api-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "@aws-cdk/core";
 import * as appSync from "@aws-cdk/aws-appsync";
 import * as lambda from "@aws-cdk/aws-lambda";
+import * as dynamodb from "@aws-cdk/aws-dynamodb";
 export class BookStoreGraphqlApiStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -19,11 +20,24 @@ export class BookStoreGraphqlApiStack extends cdk.Stack {
       },
     });
 
+    const booksTable = new dynamodb.Table(this, "BooksTable", {
+      partitionKey: {
+        name: "id",
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
     const listBooksLambda = new lambda.Function(this, "listBooksHandler", {
       code: lambda.Code.fromAsset("functions"),
       runtime: lambda.Runtime.NODEJS_14_X,
       handler: "listBooks.handler",
+      environment: {
+        BOOKS_TABLE: booksTable.tableName,
+      },
     });
+
+    booksTable.grantReadData(listBooksLambda);
 
     const listBooksDataSource = api.addLambdaDataSource(
       "listBooksDataSource",
